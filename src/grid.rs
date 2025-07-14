fcf82a9c8d3b48053348b80a6bd78c7af3037dba
Matan Lurey <matan@lurey.org>
2025-07-14 14:10:31 -0700

    Remove GridUnchecked for now

--- src/grid.rs
+++ src/grid.rs
@@ -17,9 +17,3 @@ pub trait Grid: GridRead + GridWrite {}
 
 /// Automatically implement `Grid` when `GridRead` + `GridWrite` are implemented.
 impl<T: GridRead + GridWrite> Grid for T {}
-
-/// A 2-dimensional grid that supports both reading and writing at specific positions without bounds checking.
-pub trait GridUnchecked: GridReadUnchecked + GridWriteUnchecked {}
-
-/// Automatically implement `GridUnchecked` when `GridReadUnchecked` + `GridWriteUnchecked` are implemented.
-impl<T: GridReadUnchecked + GridWriteUnchecked> GridUnchecked for T {}
